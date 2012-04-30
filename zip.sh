9d468e74a87e391f5c95d2ef6d5f0155209cd4dd
Elia Zanaboni <elia.zanaboni@gmail.com>
2012-04-30 18:28:29 +0200

    Added distributable directory, removed relative paths from zip

---
+++ zip.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ../
+rm -rf ASmallLife/distr/asmalllife.zip
+zip -r ASmallLife/distr/asmalllife.zip ASmallLife/bin/ ASmallLife/data/ ASmallLife/libs/ ASmallLife/README.md ASmallLife/start.bat
