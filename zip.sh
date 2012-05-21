98e3842e82e8b035ea899deaa128061270060362
Elia Zanaboni <elia.zanaboni@gmail.com>
2012-05-21 18:04:49 +0200

    Rappo mesh cleaned

--- zip.sh
+++ zip.sh
@@ -2,4 +2,3 @@
 cd ../
 rm -rf ASmallLife/distr/asmalllife-0.5.zip
 zip -r ASmallLife/distr/asmalllife-0.5.zip ASmallLife/bin/ ASmallLife/data/*.jpg ASmallLife/data/*.png ASmallLife/data/*.obj ASmallLife/data/*.md5mesh ASmallLife/data/*.md5anim ASmallLife/libs/ ASmallLife/README.md ASmallLife/start_win32.bat ASmallLife/start_linux.sh
-
